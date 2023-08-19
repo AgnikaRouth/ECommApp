3b80008289ff67a86995bbeaa1ff332387c596c7
AgnikaR <agnika@codebuddy.co>
2023-08-20 00:49:57 +0530

    register, login, jwtAccessToken

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const userRoute = require('./routes/user');
-
+const authRoute = require('./routes/auth');
 dotenv.config();
 
 mongoose.set('strictQuery', false);
@@ -17,7 +17,9 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Backend server is running!');
