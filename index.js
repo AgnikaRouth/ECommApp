1fce2ff525c3deb0eddd8c17fa0eaccef7d0f607
AgnikaR <agnika@codebuddy.co>
2023-08-24 13:10:48 +0530

    CRUD for products,orders and users

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+
 dotenv.config();
 
 mongoose.set('strictQuery', false);
@@ -20,6 +24,9 @@ app.use(express.json());
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Backend server is running!');
