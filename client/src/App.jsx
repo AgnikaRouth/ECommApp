f341d7aa2f9cf92d93353fae2f46c5981345e227
AgnikaR <agnika@codebuddy.co>
2023-10-29 22:18:37 +0530

    run vite on localhost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Router } from 'react-router-dom';
+// import { Switch } from 'react-router';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -6,7 +8,31 @@ import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 
 const App = () => {
-	return <Cart />;
+	return (
+		<Home />
+		// <Router>
+		// 	{/* <Switch> */}
+		// 	<Route exact path='/'>
+		// 		{' '}
+		// 		<Home />
+		// 	</Route>
+		// 	<Route path='/products/:category'>
+		// 		<ProductList />
+		// 	</Route>
+		// 	<Route path='/product/:id'>
+		// 		<Product />
+		// 	</Route>
+		// 	<Route path='/cart'>
+		// 		<Cart />
+		// 	</Route>
+		// 	{/* <Route path='/success'>
+		// 		<Success />
+		// 	</Route> */}
+		// 	{/* Login */}
+		// 	{/* Register */}
+		// 	{/* </Switch> */}
+		// </Router>
+	);
 };
 
 export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import dns from 'node:dns';
 
+// dns.setDefaultResultOrder('ipv4first');
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: 'localhost',
+		port: 3000,
+	},
+});
+
